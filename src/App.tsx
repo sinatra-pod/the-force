cae07f808014606aa3328ba03b910173722448ee
mutuajoseph <wambuajoseph756@gmail.com>
2023-01-24 07:41:11 +0300

    Chore: Initialized the project, removed unwanted files & configured tailwind.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-red-400 text-4xl flex justify-center items-center h-[100vh] text-center">
+      Lets Build The Force Platform
     </div>
   );
 }
