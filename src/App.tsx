db7e4d66606132002ac65728261db4efec619881
Titus Muthomi <muthomititus+github@gmail.com>
2023-01-25 09:10:26 +0300

    FIX: Install packages to resolve package error

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./views/Home";
+import Register from "./views/Register";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
