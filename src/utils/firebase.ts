04efa52466fa02aae783005cd8f54bbfa9a32c97
Albert Byrone <albertbyrone1677@gmail.com>
2023-01-31 11:41:27 +0300

    chore:import environment variables

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,17 +1,14 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from "firebase/auth";
 
-
-// import * as  process
-console.log("key", process.env)
 const firebaseConfig = {
-  apiKey: "AIzaSyDBImwCpYJYbPOPxu27ABqcQgqEm9pQ-CM",
-  authDomain: "the-force-8688d.firebaseapp.com",
-  projectId: "the-force-8688d",
-  storageBucket: "the-force-8688d.appspot.com",
-  messagingSenderId: "1012241051138",
-  appId: "1:1012241051138:web:14b3786518e5b2a81d011b",
-  measurementId: "G-NBY1FE4714"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
