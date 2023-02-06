663148c51b8fa5bf45f8348ee49a33ce26fc9877
kalunge <muthomititus+github@gmail.com>
2023-02-06 11:42:25 +0300

    Navigate user to home on successful login

--- src/hooks/useLoginWithGithub.ts
+++ src/hooks/useLoginWithGithub.ts
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import { GithubAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useCallback, useState } from "react";
@@ -5,6 +6,7 @@ import { useCallback, useState } from "react";
 export const useLoginWithGithub = () => {
   const [error, setError] = useState("");
   const [isPending, setIsPending] = useState(false);
+  const navigate = useNavigate()
 
   const loginWithGithub = useCallback(async () => {
     setError("");
@@ -16,17 +18,17 @@ export const useLoginWithGithub = () => {
         if(!res.user){
             throw new Error("No user found");
         }
-
         const user = res.user
         console.log(user)
         setIsPending(false);
+        navigate("/");
     } catch (err:  any) {
         console.log(err)
         setError(err.message);
         setIsPending(false);
     }
-  }, []);
-  
+  }, [navigate ]);
+
   return { loginWithGithub
     , error, isPending };
 };
