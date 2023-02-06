4cbae38e8f7edc5ed8be92d160959d6f95fa607b
kalunge <muthomititus+github@gmail.com>
2023-02-06 10:45:28 +0300

    complete and export login, error and pending functionalities

--- src/hooks/useLoginWithGithub.ts
+++ src/hooks/useLoginWithGithub.ts
@@ -1,14 +1,33 @@
-import {GithubAuthProvider, signInWithPopup} from 'firebase/auth'
-import {auth} from '../utils/firebase'
-import {useCallback} from 'react'
+import { GithubAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useCallback, useState } from "react";
 
 export const useLoginWithGithub = () => {
-    const loginWithGithub = useCallback(async () => {
-        const provider = new GithubAuthProvider()
-        const result = await signInWithPopup(auth, provider)
-        return result
-    }, [])
-    return loginWithGithub
-    }
+  const [error, setError] = useState("");
+  const [isPending, setIsPending] = useState(false);
 
-// Path: src/pages/Login.tsx
+  const loginWithGithub = useCallback(async () => {
+    setError("");
+    setIsPending(true);
+    const provider = new GithubAuthProvider();
+    try {
+      const res = await signInWithPopup(auth, provider);
+        console.log(res);
+        if(!res.user){
+            throw new Error("No user found");
+        }
+
+        const user = res.user
+        console.log(user)
+        setIsPending(false);
+      
+    } catch (err:  any) {
+        console.log(err)
+        setError(err.message);
+        setIsPending(false);
+    }
+  }, []);
+  return { loginWithGithub
+    , error, isPending };
+};
+
