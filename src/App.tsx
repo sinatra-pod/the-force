7bf608d1f0cb1d5ca55498ee14c0ea6994f0da36
Machoka Daisy <machokadaisy25@gmail.com>
2023-02-22 13:55:08 +0300

    add route for single person view

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Home from "./views/Home";
 import Register from "./views/Register";
 import Login from "./views/Login";
+import Person from "./views/Person";
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
+      <Route path ="/persons" element={<Person/>}>
+        <Route path="/persons/:id" element={<Person/>}/>
+      </Route>
     </Routes>
   );
 }
