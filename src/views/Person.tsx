e64145b483a60b91f94f567d514a2f95ec6cb4e5
Machoka Daisy <machokadaisy25@gmail.com>
2023-03-01 19:48:44 +0300

    change images and add icons

--- src/views/Person.tsx
+++ src/views/Person.tsx
@@ -1,3 +1,5 @@
+import {FaQuoteLeft, FaQuoteRight} from "react-icons/fa"
+
 
 function Person(){
     return (
@@ -5,12 +7,12 @@ function Person(){
             <div className="person-section">
                 <div className="card-section">
                     <div className="flex justify-center items-center w-100%">
-                        <div className="flex flex-col md:flex-row max-w-[80%] m-6 rounded-lg bg-gray-800 shadow-lg">
-                            <img className=" w-full h-[500px] md:h-[600px] object-cover md:w-[600px] rounded-t-lg md:rounded-none md:rounded-l-lg" src="https://images.pexels.com/photos/14561255/pexels-photo-14561255.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+                        <div className="flex flex-col md:flex-row max-w-[80%] m-6 rounded-lg shadow-lg" style={{ backgroundColor: "#423E3E" }}>
+                            <img className=" w-full h-[500px] md:h-[600px] object-cover md:w-[600px] rounded-t-lg md:rounded-none md:rounded-l-lg" src="https://images.pexels.com/photos/4310574/pexels-photo-4310574.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Darth Vader" />
                             <div className="p-6 flex flex-col justify-start">
                                 <h5 className="text-gray-500 text-xl font-medium mb-2">Darth Vader</h5>
-                                <p className="text-white-700 text-base mb-4">Once the heroic Jedi Knight named Anakin Skywalker, Darth Vader was seduced by the dark side of the Force. Forever scarred by his defeat on Mustafar, Vader was transformed into a cybernetically-enhanced Sith Lord. At the dawn of the Empire, Vader led the Empire’s eradication of the Jedi Order and the search for survivors. He remained in service of the Emperor -- the evil Darth Sidious -- for decades, enforcing his Master’s will and seeking to crush the Rebel Alliance and other detractors. But there was still good in him…</p>
-                                <p className="text-yellow-300 text-xs italic">"Luke, I am your father"</p>
+                                <p className="text-white-700 text-base mb-10">Once the heroic Jedi Knight named Anakin Skywalker, Darth Vader was seduced by the dark side of the Force. Forever scarred by his defeat on Mustafar, Vader was transformed into a cybernetically-enhanced Sith Lord. At the dawn of the Empire, Vader led the Empire’s eradication of the Jedi Order and the search for survivors. He remained in service of the Emperor -- the evil Darth Sidious -- for decades, enforcing his Master’s will and seeking to crush the Rebel Alliance and other detractors. But there was still good in him…</p>
+                                <p className="text-yellow-300 text-xs italic "><FaQuoteLeft/> Luke, I am your father<FaQuoteRight/></p>
                             </div>
                         </div>
                     </div>
@@ -18,23 +20,23 @@ function Person(){
                 <div className="data-section mb-[20px] mt-[20px]">
 
                     <div className="flex flex-col justify-center">
-                    <h2 className="p-6 text-xl font-large">STATS</h2>
-                    <div className="grid grid-cols-1 md:grid-cols-4 gap-4 justify-items-start " >
-                        <div className="flex flex-col items-center p-6">
+                    <h2 className="p-6 text-xl font-large ">STATS</h2><hr></hr>
+                    <div className="grid grid-cols-1 md:grid-cols-4 justify-items-start md:justify-items-center " >
+                        
                             
                             
-                            <div className="items-start">
-                                <h3 className="text-gray-700 text-xl font-medium mb-2">Origin</h3>
+                            <div className="items-start p-6">
+                                <h3 className="text-gray-500 hover:text-yellow-300 hover:underline hover:decoration-4 hover:decoration-yellow-300 text-xl font-medium mb-2">Origin</h3>
                                 <p>Species: Human</p>
                                 <p> Homeworld: Tatooine</p>
                                 <p>Birth Year: 41.9 BBY</p>
                                 <p>Gender: Male</p>
                             </div>
                             
-                        </div>
-                        <div className="flex flex-col items-center p-6">
-                            <div className="items-start">
-                            <h3 className="text-gray-700 text-xl font-medium mb-2">Appearance</h3>
+                        
+                        
+                            <div className="items-start p-6">
+                            <h3 className="text-gray-500 hover:text-yellow-300 hover:underline hover:decoration-4 hover:decoration-yellow-300 text-xl font-medium mb-2">Appearance</h3>
                                 <p>Height: 202</p>
                                 <p> Mass: 136</p>
                                 <p>Eye color: Yellow</p>
@@ -42,10 +44,8 @@ function Person(){
                                 <p>Skin color: White </p>
                             </div>
                             
-                        </div>
-                        <div className="flex flex-col items-center p-6">
-                            <div className="items-start">
-                            <h3 className="text-gray-700 text-xl font-medium mb-2">Films</h3>
+                            <div className="items-start p-6">
+                            <h3 className="text-gray-500 hover:text-yellow-300 hover:underline hover:decoration-4 hover:decoration-yellow-300  text-xl font-medium mb-2">Films</h3>
                             <p>Star Wars: A New Hope</p>
                             <p>Star Wars: The Empire Strikes Back</p>
                             <p>Star Wars: Return of the Jedi</p>
@@ -53,13 +53,10 @@ function Person(){
 
                             </div>
                             
-                        </div>
-                        <div className="flex flex-col items-center p-6">
-                            <div className="items-start">
-                            <h3 className="text-gray-700 text-xl font-medium mb-2">Databank</h3>
+                            <div className="items-start p-6">
+                            <h3 className="text-gray-500 hover:text-yellow-300 hover:underline hover:decoration-4 hover:decoration-yellow-300 text-xl font-medium mb-2">Databank</h3>
                             <p>Vehicles: TIE Advanced x1</p>
                             </div>
-                        </div>
 
                     </div>
                     </div>
@@ -67,37 +64,37 @@ function Person(){
                 </div>
 
             </div>
-            <div className="others mb-[20px]">
+            <div className="others">
                 <div className="flex flex-col">
-                    <h3 className="p-6 text-xl font-large">MORE CHARACTERS</h3>
-                    <div className="grid grid-cols-1 md:grid-cols-4 gap-4 justify-center">
+                    <h3 className="p-6 text-xl font-large ">MORE CHARACTERS</h3><hr />
+                    <div className="grid grid-cols-1 md:grid-cols-4 gap-4 justify-items-center">
                        
                         <div className="flex justify-center items-center">
-                            <div className="max-w-lg justify-center m-2">
-                                <img className="h-[300px] object-cover max-w-full rounded-lg" src="https://images.pexels.com/photos/7829101/pexels-photo-7829101.jpeg?auto=compress&cs=tinysrgb&w=600" alt="c3po"/>
-                                <p className="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Yoda</p>
+                            <div className="max-w-lg justify-center mt-20">
+                                <img className="h-[300px] object-cover w-[250px] rounded-lg" src="https://thumbs.dreamstime.com/b/star-wars-identities-exhibition-ottawa-canada-july-over-costumes-props-models-artwork-canada-july-34010411.jpg" alt="c-3po"/>
+                                <p className="mt-2 mb-10 text-sm text-center text-gray-500 dark:text-gray-400">C-3PO</p>
                             </div>
 
                         </div>
                         <div className="flex justify-center items-center" >
 
-                        <div className="max-w-lg justify-center m-2 ">
-                            <img className="h-[300px] max-w-full rounded-lg" src="https://images.pexels.com/photos/7829101/pexels-photo-7829101.jpeg?auto=compress&cs=tinysrgb&w=600" alt="yoda"/>
-                            <p className="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Yoda</p>
+                        <div className="max-w-lg justify-center mt-20">
+                            <img className="h-[300px] object-cover w-[250px] rounded-lg" src="https://images.pexels.com/photos/3526020/pexels-photo-3526020.jpeg?auto=compress&cs=tinysrgb&w=600" alt="chewbacca"/>
+                            <p className="mt-2 mb-10 text-sm text-center text-gray-500 dark:text-gray-400">Chewbacca</p>
                         </div>
 
                         </div>
                         <div className="flex justify-center items-center">
-                            <div className="max-w-lg justify-center m-2">
-                                <img className="h-[300px] max-w-full rounded-lg" src="https://images.pexels.com/photos/7829101/pexels-photo-7829101.jpeg?auto=compress&cs=tinysrgb&w=600" alt="leah"/>
-                                <p className="mt-2 text-center text-sm  text-gray-500 dark:text-gray-400">Yoda</p>
+                            <div className="max-w-lg justify-center mt-20">
+                                <img className="h-[300px] object-cover w-[250px] rounded-lg" src="https://images.pexels.com/photos/7829101/pexels-photo-7829101.jpeg?auto=compress&cs=tinysrgb&w=600" alt="yoda"/>
+                                <p className="mt-2 mb-10 text-center text-sm  text-gray-500 dark:text-gray-400">Yoda</p>
                             </div>
 
                         </div>
                         <div className="flex justify-center items-center">
-                            <div className="max-w-lg justify-center m-2">
-                                <img className="h-[300px] max-w-full rounded-lg" src="https://images.pexels.com/photos/7829101/pexels-photo-7829101.jpeg?auto=compress&cs=tinysrgb&w=600" alt=""/>
-                                <p className="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Yoda</p>
+                            <div className="max-w-lg justify-center mt-20 ">
+                                <img className="h-[300px] object-cover w-[250px] rounded-lg" src="https://thumbs.dreamstime.com/b/grayscale-girl-cosplaying-princess-leia-star-wars-fantasy-fest-fuenlabrada-261658065.jpg" alt="princess leia"/>
+                                <p className="mt-2 mb-10 text-sm text-center text-gray-500 dark:text-gray-400">Princess Leia</p>
                             </div>
 
                         </div>
