2caba3bae5b7d585d558e5e76629592b542cea6e
Ian Okumu <okumu.otsembo@gmail.com>
2023-03-02 10:07:29 +0300

    Tmp: Fix grid flex

--- src/views/Starships.tsx
+++ src/views/Starships.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import AltLogo from "../images//starwars_alt_logo.jpg"
 export function Starships(){
 
-    const ships = [1,1,1,1,1,1,1,1,1].map((value) => {
+    const ships = [1,1,1,1,1].map((value) => {
         return(
             <StarshipItem logo={AltLogo} title={'Starwars welcome to galaxy'} description={'Star wars is a space opera from chise created by George'} />
         )
     })
 
     return (
-        <div className={'flex flex-row'}>
+        <div className={'grid grid-cols-1 gap-6 md:grid-cols-3 lg:grid-cols-5'}>
             { ships }
         </div>
     )
@@ -17,7 +17,7 @@ export function Starships(){
 
 function StarshipItem ({ logo,  title, description }) {
     return (
-        <div className={'flex flex-col sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 m-5'}>
+        <div className={'flex flex-col'}>
             <img className={'object-cover h-96'} src={logo} alt={''}/>
             <h1 className={'text-2xl px-1 text-white'}>
                 {title}
