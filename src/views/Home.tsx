55755c1278a50600062035961af52249c6ea54da
Albert Byrone <albertbyrone1677@gmail.com>
2023-03-02 16:49:19 +0300

    remove unused components

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { getUser } from "../utils/authData";
 import { useNavigate } from "react-router-dom";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 import FilmsList from "../components/Films/FilmsList";
 
 const films = [
