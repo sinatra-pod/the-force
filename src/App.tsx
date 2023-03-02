5c87500fed3e36c0391aae0af47f9631214ab2b3
Albert Byrone <albertbyrone1677@gmail.com>
2023-03-02 15:12:11 +0300

    import planet and databanks components

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,40 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import "./App.css";
 import Home from "./views/Home";
 import Register from "./views/Register";
 import Login from "./views/Login";
 import FilmDetail from "./views/FilmDetail";
-import Planets from "./views/Planets";
+import Planets from "./components/planets/Planets";
+
+import PlanetDetails from "./components/planets/PlanetDetails";
+import Header from "./components/Header";
+import Databank from "./components/databanks/Databank";
+import DatabankDetails from "./components/databanks/DatabankDetails";
 
 function App() {
+  const { pathname } = useLocation();
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/filmdetail" element={<FilmDetail />} />
-      <Route path="/planets" element={<Planets />} />
-    </Routes>
+    <>
+      <header className="container__header">
+        {pathname !== "/login" && <Header /> && pathname !== "/register" && <Header />}
+      </header>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/filmdetail" element={<FilmDetail />} />
+          <Route path="/planets" element={<Planets />} />
+          <Route path="/planet/:id" element={<PlanetDetails />} />
+          <Route path="/databanks" element={<Databank />} />
+          <Route path="/databank/:id" element={<DatabankDetails />} />
+
+        </Routes>
+      </div>
+    </>
+
   );
 }
 
