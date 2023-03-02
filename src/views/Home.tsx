4435a5b5474a158bb87f009324bb0d3532e03b98
otsembo <okumu.otsembo@gmail.com>
2023-03-02 16:56:59 +0300

    Home: Reset original ui

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import useHeader from "../hooks/useHeader";
 import {getUser} from "../utils/authData";
 import {useNavigate} from "react-router-dom";
-import {Starships} from "./Starships";
 
 function Home() {
 
@@ -17,13 +16,9 @@ function Home() {
   const { header } = useHeader("Home");
 
   return (
-      <div className={"text-red-400"}>
-          <Starships/>
-      </div>
- /*   <div className="text-red-400 text-4xl flex justify-center items-center h-[100vh] text-center">
-      {/!*This the {header} Page*!/}
-
-    </div>*/
+    <div className="text-red-400 text-4xl flex justify-center items-center h-[100vh] text-center">
+      This the {header} Page
+    </div>
   );
 }
 
